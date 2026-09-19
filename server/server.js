18914055abd5303a72a3843fd7a3e823404ac9bd
Furqan Ali <furqanalyh@gmail.com>
2026-09-20 00:40:33 +0500

    Set up the Express backend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "CyberX Soft API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `CyberX Soft backend running on http://localhost:${PORT}`
+  );
+});
